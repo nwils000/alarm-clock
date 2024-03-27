355430dc14a527950c23702a6ff678f2839cffac
nwils000 <nwils000@gmail.com>
2024-03-26 21:44:00 -0400

    reorganized pseudocode

--- script.js
+++ script.js
@@ -16,26 +16,29 @@ let submitSetAlarm = document.querySelector('.submit-set-alarm');
 let snooze = document.querySelector('.snooze');
 let dismiss = document.querySelector('.dismiss');
 
+let audio = document.createElement('audio');
 /* 
 variables: 
-clock: to hide when timer goes off. 
-alarm: to display when timer goes off.
-time: to add the current time
-date: to add the current date
-day: to add the current day
-temperature: to add the current temperature
 
-setAlarm: display when user clicks toggleSetAlarm
-hour: interpret the hour input
-minute: interpret the minute input
 userAlarmTime: combine the hour and minute into usable data
-reason: use to display in .alarm-reason element
 
-sound: use to decide what source to give to an audio element
-alarmReason: the h2 element that needs to be populated with reason
-audio: use to create an audio element
-submitSetAlarm: use to toggle the clock and alarm displays and pass the correct data to alarm
-snooze: add new alarm with teh exact same data for 5 minutes
-dismiss: remove the alarm and redisplay the clock
+
+COMPLETED:
+*clock: to hide when timer goes off. 
+*alarm: to display when timer goes off.
+*time: to add the current time
+*date: to add the current date
+*day: to add the current day
+*temperature: to add the current temperature
+*setAlarm: display when user clicks toggleSetAlarm
+*hour: interpret the hour input
+*minute: interpret the minute input
+*reason: use to display in .alarm-reason element
+*sound: use to decide what source to give to an audio element
+*alarmReason: the h2 element that needs to be populated with reason
+*audio: use to create an audio element
+*submitSetAlarm: use to toggle the clock and alarm displays and pass the correct data to alarm
+*snooze: add new alarm with teh exact same data for 5 minutes
+*dismiss: remove the alarm and redisplay the clock
 
 */
